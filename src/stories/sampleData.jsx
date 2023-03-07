b1a7099ece631a35d5c4505abbefac254f1e6165
Yiming Hu <20092158@mail.wit.ie>
2023-03-07 23:39:57 +0000

    Fix broken stories in Storybook.

--- src/stories/sampleData.jsx
+++ src/stories/sampleData.jsx
@@ -1,3 +1,4 @@
+
 export default {
     adult: false,
     backdrop_path: "/5Iw7zQTHVRBOYpA0V6z0yypOPZh.jpg",
@@ -26,25 +27,6 @@ export default {
         name: "Action",
       },
     ],
-    production_countries: [
-        {
-            id: 19,
-            name: "Croatia",
-          },
-          {
-            id: 20,
-            name: "Ireland",
-          },
-          {
-            id: 21,
-            name: "Bolivia",
-          },
-          {
-            id: 22,
-            name: "Mexico",
-          },
-    
-      ],
     homepage:
       "https://www.starwars.com/films/star-wars-episode-viii-the-last-jedi",
     id: 181808,
@@ -97,3 +79,4 @@ export default {
     vote_average: 7,
     vote_count: 9692,
   };
+  
